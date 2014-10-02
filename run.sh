9e698205a88663e2a8d0aa887ed2260b38fa3dc4
fly1tkg <fly1tkg@gmail.com>
2014-10-02 16:16:05 +0900

    fix init file path

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 template_init_file=$WERCKER_STEP_ROOT/redis.rb
 
-init_file="$PWD/config/database.yml"
+init_file="$PWD/config/initiarizers/redis.rb"
 
 if [ -f "$init_filename" ]; then
   warn 'config/initiarizers/redis.rb already exists and will be overwritten'
