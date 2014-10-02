f1aef9fa56c3aca052b6d293357ec4986e9196ea
fly1tkg <fly1tkg@gmail.com>
2014-10-02 16:28:39 +0900

    fix typo

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 template_init_file=$WERCKER_STEP_ROOT/redis.rb
 
-init_file="$PWD/config/initiarizers/redis.rb"
+init_file="$PWD/config/initializers/redis.rb"
 
 if [ -f "$init_filename" ]; then
-  warn 'config/initiarizers/redis.rb already exists and will be overwritten'
+  warn 'config/initializers/redis.rb already exists and will be overwritten'
 fi
 
 cp -f $template_init_file $init_file
